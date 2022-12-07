713822db63246b97b3c5f4c8fc7b0861152686a2
Galdea <gabriel.aldea@gmail.com>
2022-12-07 20:07:54 +0000

    proyecto finalizado, salvo por ratio wh proporcional

--- src/app.js
+++ src/app.js
@@ -7,5 +7,59 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var minNumber = 1; // The minimum number you want
+  var maxNumber = 3; // The maximum number you want
+  var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber); // Generates random number
+  if (randomnumber == 1) {
+    randomnumber = "♥";
+    document.getElementsByClassName("topcardsymbol")[0].style.color = "red";
+    document.getElementsByClassName("bottomcardsymbol")[0].style.color = "red";
+  }
+  if (randomnumber == 2) {
+    randomnumber = "♦";
+    document.getElementsByClassName("topcardsymbol")[0].style.color = "red";
+    document.getElementsByClassName("bottomcardsymbol")[0].style.color = "red";
+  }
+  if (randomnumber == 3) {
+    randomnumber = "♣";
+    document.getElementsByClassName("topcardsymbol")[0].style.color = "black";
+    document.getElementsByClassName("bottomcardsymbol")[0].style.color =
+      "black";
+  }
+  if (randomnumber == 4) {
+    randomnumber = "♠";
+    document.getElementsByClassName("bottomcardsymbol")[0].style.color =
+      "black";
+  }
+  document.getElementsByClassName("topcardsymbol")[0].innerHTML = randomnumber;
+  document.getElementsByClassName(
+    "bottomcardsymbol"
+  )[0].innerHTML = randomnumber;
+
+  return false; // Returns false just to tidy everything up
+};
+
+const prev_handler = window.onload;
+window.onload = function() {
+  if (prev_handler) {
+    prev_handler();
+    // write your code here
+    var minNumber = 1; // The minimum number you want
+    var maxNumber = 12; // The maximum number you want
+    var randomnumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber); // Generates random number
+    if (randomnumber == 1) {
+      randomnumber = "A";
+    }
+    if (randomnumber == 11) {
+      randomnumber = "J";
+    }
+    if (randomnumber == 12) {
+      randomnumber = "Q";
+    }
+    if (randomnumber == 13) {
+      randomnumber = "K";
+    }
+    document.getElementById("myNumber").innerHTML = randomnumber;
+    return false; // Returns false just to tidy everything up
+  }
 };
